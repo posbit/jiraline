43ef5a38c6a408f254d6733e9275f0326987a604
Marek Marecki <marekjm@ozro.pw>
2016-09-25 13:05:48 +0200

    Better formatting of comment message displayed in editor

--- jiraline.py
+++ jiraline.py
@@ -645,8 +645,8 @@ def commandComment(ui):
             'text': initial_comment_text,
         }
         if cached.is_cached():
-            fmt['issue_summary'] = cached.get('fields.summary', default=summary_not_available).strip()
-            fmt['issue_description'] = '\n'.join(['#   {}'.format(_) for _ in cached.get('fields.description', default=description_not_available).strip().splitlines()])
+            fmt['issue_summary'] = get_nice_wall_of_text(cached.get('fields.summary', default=summary_not_available).strip(), indent='#   ')
+            fmt['issue_description'] = get_nice_wall_of_text(cached.get('fields.description', default=description_not_available).strip(), indent='#   ')
         if '--reply' in ui and cached.is_cached():
             comments = cached.get('fields', 'comment', default={}).get('comments', [])
             if comments:
