b1bcf1c7b1159953896a62fb746e3f228f8fc544
Marek Marecki <marek.marecki@posbit.pl>
2016-12-20 16:26:10 +0100

    Better formatting

--- jiraline.py
+++ jiraline.py
@@ -1255,7 +1255,7 @@ def commandOpen(ui):
         if not description.strip():
             description = get_message_from_editor('issue_open_message_description', {
                 'summary': get_nice_wall_of_text(summary, indent='#   '),
-            })
+            }, join_lines='\n')
         if not description.strip():
             print('error: aborting due to empty description')
             exit(1)
